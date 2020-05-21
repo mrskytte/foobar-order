7ad6e92eaeaa6f9592d26b5ffa6cf34be2981869
Chris <mr.skytte@me.com>
2020-05-21 13:51:46 +0200

    fetched local json beer list

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,18 @@
 import React, { useEffect } from "react";
 import Card from "./Card";
+import beers from "./beers.json";
 
 export default function Main(props) {
   const endpoint = "https://fireoranges.herokuapp.com";
-
+  console.log(beers.Steampunk);
   useEffect(() => {
     fetchData();
     async function fetchData() {
       const data = await fetch(endpoint);
+      console.log(data);
       const response = await data.json();
-      console.log(response.taps);
+      console.log(response.taps[1]);
+      console.log(beers[response.taps[1].beer]);
     }
   });
   return (
