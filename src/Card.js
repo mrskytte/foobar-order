d0bfac2b2677f9dab8dd1a1decf3f4d95c6f52e7
Chris <mr.skytte@me.com>
2020-05-22 10:31:23 +0200

    commit

--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 export default function Card(props) {
+  const beerInfo = props[1];
+  console.log(beerInfo.popular);
   function classNames(classes) {
     return Object.entries(classes)
       .filter(([key, value]) => value)
@@ -10,18 +12,19 @@ export default function Card(props) {
 
   const classes = {
     "card-element": true,
-    "beers-ordered": props.beersOrdered > 0,
-    "card-active": props.cardActive,
+    "beers-ordered": beerInfo.beersOrdered > 0,
+    "card-active": beerInfo.cardActive,
   };
 
   const myClassNames = classNames(classes);
   return (
-    <article id={props.beerName} className={myClassNames}>
-      <img src={props.img} alt="Image of Beer" />
-      <h2>{props.beerName}</h2>
-      <p>{props.percentage}, 0.5l</p>
-      <p>
-        {props.beerPrice} <span>{props.popular ? "⭐" : ""}</span>
+    <article id={beerInfo.name} className={myClassNames}>
+      <img src={beerInfo.img} alt="Beer" />
+      <h2>{beerInfo.name}</h2>
+      <p className="alc">{beerInfo.alc}%, 0.5l</p>
+      <p className="price">
+        {beerInfo.price}kr{" "}
+        <span>{beerInfo.popular === "true" ? "⭐" : ""}</span>
       </p>
     </article>
   );
--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,48 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Card from "./Card";
 import beers from "./beers.json";
+import Button from "./Button";
+import Checkout from "./Checkout";
+
+const endpoint = "https://fireoranges.herokuapp.com";
 
 export default function Main(props) {
-  const endpoint = "https://fireoranges.herokuapp.com";
-  console.log(beers.Steampunk);
+  const beersArray = Object.entries(beers);
+  let beersOnTap = "";
+
+  const [cards, setCards] = useState([]);
+
   useEffect(() => {
     fetchData();
     async function fetchData() {
       const data = await fetch(endpoint);
-      console.log(data);
       const response = await data.json();
-      console.log(response.taps[1]);
-      console.log(beers[response.taps[1].beer]);
+      const onTap = Object.values(response.taps);
+      let test = [];
+      onTap.forEach((oneTap) => {
+        test.push(oneTap.beer);
+      });
+      beersOnTap = beersArray.filter((beer) => {
+        if (test.includes(beer[0])) {
+          return beer;
+        }
+        return;
+      });
+      setCards(beersOnTap);
     }
-  });
+  }, []);
+
+  console.log("hello");
+  const cardsInUse = cards.map((c) => <Card {...c} key={c[0]} />);
   return (
     <main>
-      <Card beerName="Hello World" />
+      <h1 className="main-title">ON TAP TODAY</h1>
+      <div>
+        <Button name="PRICE" />
+        <Button name="POPULAR" />
+      </div>
+      {cardsInUse}
+      <Checkout />
     </main>
   );
 }
