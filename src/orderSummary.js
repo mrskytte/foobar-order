274e70b8e0167211586fd41cea6a2eab52cf21ae
Chris <mr.skytte@me.com>
2020-05-27 19:09:00 +0200

    console logs removed

--- src/orderSummary.js
+++ src/orderSummary.js
@@ -1,18 +1,14 @@
 import React from "react";
 
 export default function OrderSummary(props) {
-  console.log("ordersummary", props);
   let total = 0;
   props.beersInOrder.forEach((oneOrder) => {
     const isBeerInOrder = props.beerInfo.filter((beer) => {
-      console.log("order Name", oneOrder);
-      console.log(beer[0]);
       if (beer[0] === oneOrder.name) {
         return beer[1];
       }
     });
-    console.log("onlyBeer", isBeerInOrder);
-    console.log("beerNPrice", isBeerInOrder[0][1].price);
+
     let price = parseInt(isBeerInOrder[0][1].price);
     total += price * oneOrder.amount;
   });
