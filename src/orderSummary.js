8cf01695ca2a4f806ae576fd69a9a9c723c5a5c2
Chris <mr.skytte@me.com>
2020-05-28 14:59:26 +0200

    button in the buttom updates to different stages

--- src/orderSummary.js
+++ src/orderSummary.js
@@ -14,7 +14,10 @@ export default function OrderSummary(props) {
   });
 
   return (
-    <button id="order-summary" onClick={props.goToOrder}>
+    <button
+      id="order-summary"
+      onClick={props.isCheckingOut ? props.goToPayment : props.goToOrder}
+    >
       {props.isCheckingOut ? "CONFIRM" : "CHECKOUT"}
       <span>TOTAL {total}kr</span>
     </button>
