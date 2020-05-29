6063a3f1248f3a57d263291f7e740c3e548fdcd5
Chris <mr.skytte@me.com>
2020-05-29 10:34:06 +0200

    minor fix

--- src/orderSummary.js
+++ src/orderSummary.js
@@ -1,18 +1,6 @@
 import React from "react";
 
 export default function OrderSummary(props) {
-  // let total = 0;
-  // props.beersInOrder.forEach((oneOrder) => {
-  //   const isBeerInOrder = props.beerInfo.filter((beer) => {
-  //     if (beer[0] === oneOrder.name) {
-  //       return beer[1];
-  //     }
-  //   });
-
-  //   let price = parseInt(isBeerInOrder[0][1].price);
-  //   total += price * oneOrder.amount;
-  // });
-
   return (
     <button
       id="order-summary"
