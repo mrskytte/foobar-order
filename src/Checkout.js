33e857e98b1c70d84eee2e03d72b6a07e44d0308
Chris <mr.skytte@me.com>
2020-06-03 13:42:27 +0200

    Empty basket message added

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Checkout(props) {
+  const [emptyBasket, setEmptyBasket] = useState("");
+
+  useEffect(() => {
+    setEmptyBasket(
+      props.cards.filter((card) => card.props[1].amount > 0).length < 1
+    );
+  }, [props.cards]);
+
   return (
     <main>
-      <h1>ORDER SUMMARY</h1>
-      <table>
-        <thead>
-          <tr>
-            <th className="summary-item">Item</th>
-            <th className="summary-price">Price</th>
-            <th className="summary-amount">Amount</th>
-          </tr>
-        </thead>
-        <tbody>{props.cards}</tbody>
-      </table>
+      {emptyBasket ? (
+        <>
+          <h1>Your basket is empty</h1>
+          <p>Go back and fill it with some delicious beer</p>
+          <button>Back to menu</button>
+        </>
+      ) : (
+        <>
+          <h1>ORDER SUMMARY</h1>
+          <table>
+            <thead>
+              <tr>
+                <th className="summary-item">Item</th>
+                <th className="summary-price">Price</th>
+                <th className="summary-amount">Amount</th>
+              </tr>
+            </thead>
+            <tbody>{props.cards}</tbody>
+          </table>
+        </>
+      )}
     </main>
   );
 }
